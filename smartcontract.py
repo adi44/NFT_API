a7a4e5da2be621f3fcd462fece9e2c32429ee1e5
Aditya Dhir <31381639+adi44@users.noreply.github.com>
2022-03-22 22:20:07 +0530

    error handling

--- smartcontract.py
+++ smartcontract.py
@@ -1,4 +1,3 @@
-from itertools import chain
 from web3 import Web3
 from web3.middleware import geth_poa_middleware
 from decouple import config
@@ -26,22 +25,47 @@ def create_nft_token_contract():
                    'nonce': web3.eth.getTransactionCount(acct._address)})
     signed_tx = web3.eth.account.signTransaction(tx, private_key)
     txn_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
-    json_response = {}
-    json_response['transaction_hash'] = str(web3.toHex(txn_hash))
-    return json_response
+    txn_reciept = web3.eth.wait_for_transaction_receipt(txn_hash)
+    if(txn_reciept['status']==1):
+        data = contract.events.NFTContractCreated().processLog(txn_reciept['logs'][0])
+        json_response = {}
+        json_response['contract_id']= web3.toChecksumAddress(data['args']['nftcontract'])
+        json_response['transaction_hash'] = str(web3.toHex(txn_hash))
+        return json_response
+    else:
+        return "failed"
 
 def mint_nft_token(contractAddress, walletAddress, metadata):
-    contract = web3.eth.contract(address = contractAddress, abi = nft_abi)
-    acct = web3.eth.account.privateKeyToAccount(private_key)
-    tx = contract.functions.mintNFT(walletAddress, metadata).buildTransaction({
+    all_deployed_tokens = all_nft_tokens()
+    json_response = {}
+    if(web3.toChecksumAddress(contractAddress) in all_deployed_tokens):
+        contract = web3.eth.contract(address = web3.toChecksumAddress(contractAddress), abi = nft_abi)
+        acct = web3.eth.account.privateKeyToAccount(private_key)
+        tx = contract.functions.mintNFT(web3.toChecksumAddress(walletAddress), metadata).buildTransaction({
                    'from': acct._address,
                    'chainId':chainId,
                    'nonce': web3.eth.getTransactionCount(acct._address)})
-    signed_tx = web3.eth.account.signTransaction(tx, private_key)
-    txn_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
-    json_response = {}
-    json_response['transaction_hash'] = str(web3.toHex(txn_hash))
-    return json_response
+        signed_tx = web3.eth.account.signTransaction(tx, private_key)
+        txn_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+        txn_reciept = web3.eth.wait_for_transaction_receipt(txn_hash)
+        if(txn_reciept['status']==1):
+            json_response = {}
+            json_response['transaction_hash'] = str(web3.toHex(txn_hash))
+            return json_response
+        else:
+            json_response['status'] = 'failed'
+            return json_response
+    else:
+        json_response['status'] = 'contract ID not deployed'
+        return json_response
+
+def all_nft_tokens():
+     contract = web3.eth.contract(address = NFTFactoryContractAddress, abi = factory_abi)
+     return contract.functions.getAlltokens().call()
+
+
+
+
 
 
 
